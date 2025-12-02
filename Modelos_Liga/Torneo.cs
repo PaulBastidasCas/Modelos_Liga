11fbe1b1c34e62ea9881bdd8ea95f622c38a262d
PaulBastidasCas <paulbastidas83@outlook.com>
2025-12-02 13:04:21 -0500

    Merge branch 'master' of https://github.com/PaulBastidasCas/Modelos_LigaModelos actualizados

---
+++ Modelos_Liga/Torneo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos_Liga
+{
+    public class Torneo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string TipoTorneo { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+
+        //public List<Inscripcion> Inscripciones { get; set; }
+        public List<Partido> Partidos { get; set; } 
+    }
+
+}
+
